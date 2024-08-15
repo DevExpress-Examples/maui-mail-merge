ba749578054f12df0f663390a57c6599e6ed0e7a
Kseniya Kuzmina <kseniya.kuzmina@devexpress.com>
2024-08-15 14:30:50 +0400

    upload new merge example

---
+++ CS/editor-data-merge/MainPage.xaml.cs
@@ -0,0 +1,66 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using DevExpress.XtraRichEdit;
+using DevExpress.XtraRichEdit.API.Native;
+
+namespace MauiOFAMerge {
+    public partial class MainPage : ContentPage {
+        public MainPage() {
+            InitializeComponent();
+        }
+        private async void OnLoaded(object sender, EventArgs e) {
+            await InitFilesAsync("Party Invitation.docx");
+        }
+        async Task InitFilesAsync(string fileName) {
+            using Stream fileStream = await FileSystem.Current.OpenAppPackageFileAsync(fileName);
+            string targetFile = Path.Combine(FileSystem.Current.AppDataDirectory, fileName);
+            using FileStream outputStream = File.OpenWrite(targetFile);
+            fileStream.CopyTo(outputStream);
+        }
+    }
+
+    public partial class MergeInfoViewModel : ObservableObject {
+        [ObservableProperty]
+        string publicName = "Alice";
+
+        [RelayCommand]
+        async Task MergeAsync() {
+            RichEditDocumentServer mergeProcessor = await RichProcFromFileAsync("Party Invitation.docx");
+            AssignSource(mergeProcessor);
+            mergeProcessor = MergeToNewDocument(mergeProcessor);
+            string docPath = await SaveToFile(mergeProcessor, "ResultingDoc.docx");
+            await ShareDocAsync(docPath);
+        }
+        async Task<RichEditDocumentServer> RichProcFromFileAsync(string fileName) {
+            string workingFilePath = Path.Combine(FileSystem.Current.AppDataDirectory, fileName);
+            RichEditDocumentServer mergeRichProcessor = new RichEditDocumentServer();
+            await mergeRichProcessor.LoadDocumentAsync(workingFilePath);
+            return mergeRichProcessor;
+        }
+        void AssignSource(RichEditDocumentServer mergeProcessor) {
+            mergeProcessor.Document.Fields[0].Locked = true;
+            mergeProcessor.Options.MailMerge.DataSource = new List<object> { new {
+                RecipientName = this.PublicName,
+                SenderCompany = "DX_Company" } };
+        }
+        RichEditDocumentServer MergeToNewDocument(RichEditDocumentServer existingDoc) {
+            RichEditDocumentServer resultDocumentProcessor = new RichEditDocumentServer();
+            resultDocumentProcessor.CreateNewDocument();
+            MailMergeOptions myMergeOptions = existingDoc.Document.CreateMailMergeOptions();
+            myMergeOptions.MergeMode = MergeMode.NewSection;
+            existingDoc.MailMerge(resultDocumentProcessor.Document);
+            return resultDocumentProcessor;
+        }
+        async Task<string> SaveToFile(RichEditDocumentServer mergeProcessor, string fileName) {
+            string fileToSavePath = Path.Combine(FileSystem.Current.AppDataDirectory, fileName);
+            await mergeProcessor.SaveDocumentAsync(fileToSavePath, DocumentFormat.OpenXml);
+            return fileToSavePath;
+        }
+        async Task ShareDocAsync(string docPath) {
+            await Share.Default.RequestAsync(new ShareFileRequest {
+                Title = "Share the file",
+                File = new ShareFile(docPath)
+            });
+        }
+    }
+}
---
+++ CS/mail-merge/ViewModel/MainViewModel.cs
@@ -0,0 +1,134 @@
+﻿using DevExpress.XtraRichEdit.API.Native;
+using DevExpress.XtraRichEdit;
+using MailMerge.Model;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Windows.Input;
+using static MailMerge.App;
+using DevExpress.XtraRichEdit.Export;
+using DevExpress.Maui.Core.Internal;
+
+namespace MailMerge.ViewModel {
+    public class MainViewModel : BindableBase {
+        ObservableCollection<Models> contacts;
+        ObservableCollection<EmailTemplate> emailTemplates;
+        Models mailToCustomer;
+        bool isTemplatesMenuVisible;
+        string currentUserName = "Alex";
+        public MainViewModel() {
+            LoadDataAsync();
+            EmailTemplates = new ObservableCollection<EmailTemplate>() {
+                new EmailTemplate("Party Invitation","Party Invitation.docx"),
+                new EmailTemplate("Financing Package ","Financing Package .docx"),
+                new EmailTemplate("Northwind University","Northwind University.docx"),
+            };
+            ShowTemplatesMenuCommand = new Command<Models>(ShowTemplatesMenu);
+            SendTemplateEmailCommand = new Command<EmailTemplate>(SendTemplateEmail);
+            CopyTemplatesAppData();
+        }
+
+        async void SendTemplateEmail(EmailTemplate template) {
+            RichEditDocumentServer resultDocumentProcessor = await MailMergeAsync(template);
+            IsTemplatesMenuVisible = false;
+            await SendOrShareResultAsync(template, resultDocumentProcessor);
+        }
+        async Task<RichEditDocumentServer> MailMergeAsync(EmailTemplate emailTemplate) {
+            string workingFilePath = Path.Combine(FileSystem.Current.AppDataDirectory, emailTemplate.DocumentSourcePath);
+            RichEditDocumentServer mergeRichProcessor = new RichEditDocumentServer();
+            await mergeRichProcessor.LoadDocumentAsync(workingFilePath);
+
+            mergeRichProcessor.Document.Fields[0].Locked = true;
+            mergeRichProcessor.Options.MailMerge.DataSource = new List<object> { new { RecipientName = mailToCustomer.FirstName, SenderName = currentUserName} };
+            MailMergeOptions myMergeOptions = mergeRichProcessor.Document.CreateMailMergeOptions();
+
+            RichEditDocumentServer resultDocumentProcessor = new RichEditDocumentServer();
+            resultDocumentProcessor.CreateNewDocument();
+            myMergeOptions.MergeMode = MergeMode.NewSection;
+            mergeRichProcessor.MailMerge(resultDocumentProcessor.Document);
+            return resultDocumentProcessor;
+        }
+        void ShowTemplatesMenu(Models customer) {
+            mailToCustomer = customer;
+            IsTemplatesMenuVisible = true;
+        }
+        async Task SendOrShareResultAsync(EmailTemplate emailTemplate, RichEditDocumentServer richProcessor) {
+            if (Email.Default.IsComposeSupported) {
+                richProcessor.BeforeExport += OnBeforeExport;
+                string htmlEmailContent = richProcessor.Document.GetHtmlText(richProcessor.Document.Range, null);
+                richProcessor.BeforeExport -= OnBeforeExport;
+                var message = new EmailMessage {
+                    Subject = emailTemplate.Name,
+                    Body = htmlEmailContent,
+                    BodyFormat = EmailBodyFormat.PlainText,
+                    To = new List<string>() { mailToCustomer.Email }
+                };
+                try {
+                    await Email.Default.ComposeAsync(message);
+                }
+                catch (Exception e){
+                    await App.Current.MainPage.DisplayAlert("Error", "Make sure your mail client is configured", "OK");
+                }
+            }
+            else {
+                string fileToSharePath = Path.Combine(FileSystem.Current.AppDataDirectory, $"{emailTemplate.Name} Result.docx");
+                await App.Current.MainPage.DisplayAlert("Can't send the email", "Email composing is not supported on this device. You can send the document with merged text to another device", "OK");
+                await richProcessor.SaveDocumentAsync(fileToSharePath, DocumentFormat.OpenXml);
+                await Share.Default.RequestAsync(new ShareFileRequest {
+                    Title = "Share the file",
+                    File = new ShareFile(fileToSharePath)
+                });
+            }
+        }
+        private void OnBeforeExport(object sender, BeforeExportEventArgs e) {
+            HtmlDocumentExporterOptions options = e.Options as HtmlDocumentExporterOptions;
+            if (options != null) {
+                options.Encoding = Encoding.UTF8;
+            }
+        }
+        async void CopyTemplatesAppData() {
+            foreach (var template in EmailTemplates) {
+                await CopyWorkingFilesToAppData(template.DocumentSourcePath);
+            }
+        }
+        public async Task CopyWorkingFilesToAppData(string fileName) {
+            using Stream fileStream = await FileSystem.Current.OpenAppPackageFileAsync(fileName);
+            string targetFile = Path.Combine(FileSystem.Current.AppDataDirectory, fileName);
+            using FileStream outputStream = File.OpenWrite(targetFile);
+            fileStream.CopyTo(outputStream);
+        }
+        public async void LoadDataAsync() {
+            var retrievedItems = await DBContactService.Instance.GetItemsAsync();
+            Contacts = new ObservableCollection<Models>(retrievedItems);
+        }
+        public ICommand SendTemplateEmailCommand {
+            get;
+            set;
+        }
+        public ICommand ShowTemplatesMenuCommand {
+            get;
+            set;
+        }
+        public ObservableCollection<Models> Contacts {
+            get { return contacts; }
+            set {
+                contacts = value;
+                RaisePropertyChanged();
+            }
+        }
+        public ObservableCollection<EmailTemplate> EmailTemplates {
+            get { return emailTemplates; }
+            set {
+                emailTemplates = value;
+                RaisePropertyChanged();
+            }
+        }
+        public bool IsTemplatesMenuVisible {
+            get { return isTemplatesMenuVisible; }
+            set {
+                isTemplatesMenuVisible = value;
+                RaisePropertyChanged();
+            }
+        }
+    }
+}
